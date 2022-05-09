c71907ab133441e379b1211e5b9d8fad29174242
Isaac Daniel Reyna <isaacdanielreyna@gmail.com>
2022-05-08 21:53:53 -0500

    added url to ALLOWED_HOSTS

--- demo/settings.py
+++ demo/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-p+^@rtu&=&a)w$u@baljo*2i#+=9)jnj9c@fpmyvbv@2p)crz-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'demo.isaacreyna.com' ]
+
 
 
 # Application definition
